8714c2ca7aaf93b8e3efdc9d146979c5cc3b2387
andySouthwick <asouthwick01@gmail.com>
2017-03-28 16:01:25 -0600

    removed vue practice code

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -5,28 +5,9 @@
 </head>
 <body>
 <div id="root">
-<button v-bind:title="title">click me</button>
-<input type="text" id="input" v-model="newName">
-<button id="button" @click="addName"> add name </button>
-<li v-for="name in names" v-text="name">
 </div>
 <script src="https://unpkg.com/vue"></script>
-<script>
-  var app = new Vue({
-      el: '#root',
-      data:{
-          title: 'JS set Title',
-          newName: "",
-          names: ['Joe', 'Mary', 'Jane', 'Jack']
-      },
-      methods: {
-          addName: function() {
-             this.names.push(this.newName);
-             this.newName = " ";
-          }
-      }
-  });
-</script>
+
 
 </body>
 </html>
