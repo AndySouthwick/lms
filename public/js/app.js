f4cc7cf02da5af916d8df9d2b77dcc974c273353
Adrien Maranville <adrien.maranville@gmail.com>
2017-04-05 21:47:35 -0600

    Added public app.js to gitignore.

--- public/js/app.js
+++ public/js/app.js
@@ -11199,7 +11199,12 @@ __webpack_require__(29);
 
 /***/ }),
 /* 10 */
+/***/ (function(module, exports) {
 
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(12);
