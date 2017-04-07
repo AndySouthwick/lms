d50909b8c1a05add13d58972715e091bf2bcf173
andySouthwick <asouthwick01@gmail.com>
2017-04-07 14:00:24 -0600

    added mycss.css

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">Dashboard</div>
 
-                    <p>Welcome {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</p>
+                    <p> Welcome {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</p>
 
                 </div>
             </div>
