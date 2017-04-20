6a55ceb7bba66e15bf0f0cd61fff496ff5caeb5f
capatten <pattenchada@gmail.com>
2017-04-19 20:19:26 -0600

    Removed Stripe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      Stripe::setApiKey(config('services.stripe.secret'));
+      //Stripe::setApiKey(config('services.stripe.secret'));
     }
 
     /**
