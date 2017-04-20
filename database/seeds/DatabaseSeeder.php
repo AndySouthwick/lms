7a631332e4f9d3cc7a7c97f9801d75462c208504
Adrien Maranville <adrien.maranville@gmail.com>
2017-04-19 19:04:18 -0600

    added plans seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(TracksTableSeeder::class);
         $this->call(CoursesTracksTableSeeder::class);
         $this->call(VideosTableSeeder::class);
-        
+        $this->call(PlansSeeder::class);
         
     }
 }
---
+++ database/seeds/PlansSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PlansSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('plans')->insert(array(
+             
+            array('name' => 'Monthly',
+            'price' => 3500),
+
+            array('name' => 'Yearly',
+            'price' => '19700')
+        ));
+    }
+}
