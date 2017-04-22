d215e10966ea519df0b0f0d84277126033f6cf03
Tyson Omer <tysonomer@Tysons-MacBook-Pro.local>
2017-04-22 17:46:38 -0600

    Added Typed Text and Css for Beautification

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -18,7 +18,62 @@
 
 <!--course listing-->
 @section('content')
-    <p>Welcome To Tech---</p>
+
+<!--Landing Page Content with scrolling text-->
+
+     <body>
+
+	<div class="banner">
+		
+		<div class="container">
+			
+				
+					
+					<div id="typed-strings" class="agileits_strings">
+                        <p>Master <i>Angular.JS</i></p>
+						<p>or perhaps <i>Laravel</i></p>
+						<p>maybe you want to brush up <br>on your <i>HTML & CSS</i></p>
+                        <p>Whatever it is we have it <i>Here.</i></p>
+					</div>
+					<span id="typed" style="white-space:pre;"></span>
+				
+			
+			
+			
+			
+	</div>
+
+
+	<script src="js/typed.js" type="text/javascript"></script>
+    <script>
+		$(function(){
+
+			$("#typed").typed({
+				// strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+				stringsElement: $('#typed-strings'),
+				typeSpeed: 30,
+				backDelay: 500,
+				loop: false,
+				contentType: 'html', // or text
+				// defaults to false for infinite loop
+				loopCount: false,
+				callback: function(){ foo(); },
+				resetCallback: function() { newTyped(); }
+			});
+
+			$(".reset").click(function(){
+				$("#typed").typed('reset');
+			});
+
+		});
+
+		function newTyped(){ /* A new typed object */ }
+
+		function foo(){ console.log("Callback"); }
+    </script>
+
+<!--Landing Page Content with scrolling text-->
+
 @endsection
 
 <!--footer-->
